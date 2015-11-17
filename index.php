b287fb8bee52ee8e3da493c2363f405bfad8f752
computeristgeek <salahuddin.kazak@gmail.com>
2015-11-17 22:39:36 +0200

    Initial index.php Change

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
-<body background="sea-road alex.jpg">
-<h1>International Conference on</h1>
+<body background="sea-road alex.jpg" style="background-repeat:repeat-y;background-size:cover;">
+<h1>International Conference on New Trends for Sustainable Energy</h1>
 <p>The Website is Coming Soon</p>
 </body>
 </html>
