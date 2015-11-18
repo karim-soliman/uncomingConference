8217625c37ee1010e38e34b1386e435e80b07525
computeristgeek <salahuddin.kazak@gmail.com>
2015-11-18 05:33:14 +0200

    Add Functionality and Links

--- index.php
+++ index.php
@@ -26,42 +26,46 @@
     <div class="container">
         <div class="row">
             <div class="col-md-9 col-xs-12" id="main"> 
-                <h1><span id="first-conf">1st INTERNATIONAL CONFERENCE ON</span><br \><span id="new-trends">NEW TRENDS FOR SUSTAINABLE ENERGY</span></h1>
+                <h1 id="title"><span id="first-conf">1st INTERNATIONAL CONFERENCE ON</span><br \><span id="new-trends">NEW TRENDS FOR SUSTAINABLE ENERGY</span></h1>
                 <h1 id="icntse">ICNTSE</h1>
-                <p class="shaded" id="1-3oct">1-3 October, 2016<br \>At Pharos University, Alexandria, Egypt</p>
-                <p class="shaded" id="1-3oct">Organized by Pharos University in Alexandria with the association of the KTH Royal Institute of Technology</p>
+                <div class="row shaded"><p class="col-xs-12" id="1-3oct">1-3 October, 2016<br \>At Pharos University, Alexandria, Egypt</p></div>
+		<div class="row shaded" id="bottombar">
+                    <p class="col-md-8 col-xs-6" id="organized">Organized by Pharos University in Alexandria with the association of the KTH Royal Institute of Technology </p>
+                    <a href="https://www.kth.se/en"><img class="col-md-2 col-xs-3" src="img/images.png" \></a>
+                    <a href="http://www.pua.edu.eg"><img class="col-md-2 col-xs-3" src="img/logo-Arabic.png" \></a>
+                </div>
             </div>
 	    <div class="col-md-3 col-xs-12" id="nav">
                     <div class="row">
 			<div class="col-md-12 col-sm-4 col-xs-6" id="about-alex">
-                            <div class="image">
-			    <img class="nav-buttons" src="img/Layer 2.png" />
+                            <a href="aboutalex.php"><div class="image">
+			    <img src="img/Layer 2.png" />
 			    <p>About Alexandria</p>
-                            </div>
+                            </div></a>
 			</div>
 			<div class="col-md-12 col-sm-4 col-xs-6" id="about-icntse">
-                            <div class="image">
-			    <img class="nav-buttons" src="img/Layer 2 copy.png" />
+                            <a href="abouticntse.php"><div class="image">
+			    <img src="img/Layer 2 copy.png" />
 			    <p>About ICNTSE</p>
-                            </div>
+                            </div></a>
 			</div>
 			<div class="col-md-12 col-sm-4 col-xs-6" id="call-for-papers">
-                            <div class="image">
-			    <img class="nav-buttons" src="img/Layer 2 copy 2.png" />
+                            <a href="callforpapers.php"><div class="image">
+			    <img src="img/Layer 2 copy 2.png" />
 			    <p>Call for Papers</p>
-                            </div>
+                            </div></a>
 			</div>
 			<div class="col-md-12 col-sm-4 col-xs-6" id="fees">
-                            <div class="image">
-			    <img class="nav-buttons" src="img/Layer 2 copy 3.png" />
+                            <a href="fees.php"><div class="image">
+			    <img src="img/Layer 2 copy 3.png" />
 			    <p>Fees</p>
-                            </div>
+                            </div></a>
 			</div>
 			<div class="col-md-12 col-sm-4 col-xs-6" id="contact-us">
-                            <div class="image">
-			    <img class="nav-buttons" src="img/Layer 2 copy 4.png" />
+                            <a href="contactus.php"><div class="image">
+			    <img src="img/Layer 2 copy 4.png" />
 			    <p>Contact Us</p>
-                            </div>
+                            </div></a>
 			</div>
                     </div>
             </div> 
