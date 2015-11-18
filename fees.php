a753abe6ad748c25fc45407d8f769993658c89eb
computeristgeek <salahuddin.kazak@gmail.com>
2015-11-18 07:18:57 +0200

    Add Fees Page

---
+++ fees.php
@@ -0,0 +1,62 @@
+<?
+//	require('bootstrap');
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>International Conference on New Trends for Sustainable Energy</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Style -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body >
+    <div class="container">
+	    <div class="row">
+		<div class="col-md-3 col-xs-12" id="feestitle">
+		    <div class="image">
+		    <img src="img/Layer 2 copy 3.png" />
+		    <p>Conference Fees</p>
+		    </div>
+		</div>
+		<div class="col-md-9 col-xs-12" id="feessubtitle">
+		    <p class="cfp_date"><span class="purple">Early booking delegates:</span> registered on or before 31 March, 2016*</p>
+		    <p class="cfp_date"><span class="purple">Standard delegates:</span> booking after 15 August, 2016**</p>
+		    <p class="cfp_date"><span class="purple"><u>Registration fee includes</u></span></p>
+		    <p class="cfp_date">Access to all conference and poster sessions</p>
+		    <p class="cfp_date">Refreshments/Lunch as advertised in the programme</p>
+		    <p class="cfp_date">Programme booklet</p>
+		    <p class="cfp_date">Online access to the general proceedings</p>
+		    </div>
+		</div>
+		<div class="col-xs-12 shadedgray" id="importantdate">
+		    <p class="white">Fees</p>
+		</div>
+		<div class="col-xs-12" id="cfpdetails">
+		    <p class="cfp_date">Authors (Early booking): $450</p>
+		    <p class="cfp_date">Authors (Standard booking): $500</p>
+		    <p class="cfp_date">On-Site Booking: $800</p>
+		    <p class="cfp_date">A Co-Author: $300</p>
+		    <p class="cfp_date">Group of Co-Authors (2-4 Co-Authors): $500</p>
+		    <p class="cfp_date">Attendee: $400</p>
+		</div>
+	    </div>
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
