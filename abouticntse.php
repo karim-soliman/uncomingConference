fd4a4b130a4c2d2defe74f73558d66522cc65822
computeristgeek <salahuddin.kazak@gmail.com>
2015-11-18 06:03:48 +0200

    Add About ICNTSE

---
+++ abouticntse.php
@@ -0,0 +1,50 @@
+<?
+//	require('bootstrap');
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>International Conference on New Trends for Sustainable Energy</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Style -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body >
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12" id="abouticntse"> 
+		<h1><span class="gray">about</span> <span class="green">ICNTSE</span></h1>
+		<p><span class="green">The conference</span> is specifically designed to focus on the issues related to energy and energy conservation. Industry leaders, engineers and academics will discuss in this conference the latest developments in the use and production of renewable energy and ways of energy conservation.</p>
+		<p><span class="green">The conference</span> will highlight important advances being made in renewable energy and energy conservation, as well as the regulatory mechanisms that help make them possible.</p>
+		<p><span class="green">The conference</span> will provide the state of the art in alternative energy sources, application technologies and implementation strategies.</p>
+		<p><span class="green">Conference Topics:</span><br />The Conference will focus on a number of themes that need prior attention, these themes include:</p>
+            </div> 
+            <div class="col-xs-12" id="icntsefields"> 
+		<ul class="green">
+			<li>Power and Energy Engineering</li>
+			<li>Renewable Energy Technologies Applications and Integration</li>
+			<li>Energy Management</li>
+			<li>Energy & Sustainable Development</li>
+		</ul>
+            </div> 
+        </div>	
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
