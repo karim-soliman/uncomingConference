aa1f7bc1ee7cbac75db87f2679c5feb536196ccc
computeristgeek <salahuddin.kazak@gmail.com>
2015-11-18 03:29:23 +0200

    Implement Mobile Design

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?
-	require('bootstrap');
+//	require('bootstrap');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,10 +8,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>International Conference on New Trends for Sustainable Energy</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Style -->
+    <link href="css/style.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -20,10 +22,51 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-  <body background="sea-road alex.jpg" style="background-repeat:repeat-y;background-size:cover;">
-    <h1>International Conference on New Trends for Sustainable Energy</h1>
-    <p>The Website is Coming Soon</p>
-
+  <body >
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9 col-xs-12" id="main"> 
+                <h1><span id="first-conf">1st INTERNATIONAL CONFERENCE ON</span><br \><span id="new-trends">NEW TRENDS FOR SUSTAINABLE ENERGY</span></h1>
+                <h1 id="icntse">ICNTSE</h1>
+                <p class="shaded" id="1-3oct">1-3 October, 2016<br \>At Pharos University, Alexandria, Egypt</p>
+                <p class="shaded" id="1-3oct">Organized by Pharos University in Alexandria with the association of the KTH Royal Institute of Technology</p>
+            </div>
+	    <div class="col-md-3 col-xs-12" id="nav">
+                    <div class="row">
+			<div class="col-md-12 col-sm-4 col-xs-6" id="about-alex">
+                            <div class="image">
+			    <img class="nav-buttons" src="img/Layer 2.png" />
+			    <p>About Alexandria</p>
+                            </div>
+			</div>
+			<div class="col-md-12 col-sm-4 col-xs-6" id="about-icntse">
+                            <div class="image">
+			    <img class="nav-buttons" src="img/Layer 2 copy.png" />
+			    <p>About ICNTSE</p>
+                            </div>
+			</div>
+			<div class="col-md-12 col-sm-4 col-xs-6" id="call-for-papers">
+                            <div class="image">
+			    <img class="nav-buttons" src="img/Layer 2 copy 2.png" />
+			    <p>Call for Papers</p>
+                            </div>
+			</div>
+			<div class="col-md-12 col-sm-4 col-xs-6" id="fees">
+                            <div class="image">
+			    <img class="nav-buttons" src="img/Layer 2 copy 3.png" />
+			    <p>Fees</p>
+                            </div>
+			</div>
+			<div class="col-md-12 col-sm-4 col-xs-6" id="contact-us">
+                            <div class="image">
+			    <img class="nav-buttons" src="img/Layer 2 copy 4.png" />
+			    <p>Contact Us</p>
+                            </div>
+			</div>
+                    </div>
+            </div> 
+        </div>	
+    </div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
