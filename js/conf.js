7789db7c219cad6b061ea7f58d4e7eb700377463
computeristgeek <salahuddin.kazak@gmail.com>
2015-11-19 10:26:04 +0200

    Fix JS Bug (setInterval, listener)

--- js/conf.js
+++ js/conf.js
@@ -9,33 +9,34 @@ function toggle_visibility(id) {
 	}
 	else console.log("element "+id+" not found");
 }
-function initHomePage()
-{
 window.addEventListener("resize", function(e) {
 	var icntse=document.getElementById("icntse");
-      	var items= document.getElementsByClassName('navmenuitem');
-	if(window.innerWidth<1000)
+	if(icntse)
 	{
-		if(window.innerWidth>800)
+		var items= document.getElementsByClassName('navmenuitem');
+		if(window.innerWidth<1000)
 		{
-			icntse.style.letterSpacing='0.125em';
-			for(i=0; i<items.length; i++) {
-				items[i].style.fontSize='1.5em';
+			if(window.innerWidth>800)
+			{
+				icntse.style.letterSpacing='0.125em';
+				for(i=0; i<items.length; i++) {
+					items[i].style.fontSize='1.5em';
+				}
+			}
+			else
+			{
+				icntse.style.letterSpacing="0em";
+				for(i=0; i<items.length; i++) {
+					items[i].style.fontSize='1em';
+				}
 			}
 		}
-		else
+		else 
 		{
-			icntse.style.letterSpacing="0em";
+			icntse.style.letterSpacing='0.5em';
 			for(i=0; i<items.length; i++) {
-				items[i].style.fontSize='1em';
+				items[i].style.fontSize='2em';
 			}
-		}
-	}
-	else 
-	{
-		icntse.style.letterSpacing='0.5em';
-		for(i=0; i<items.length; i++) {
-			items[i].style.fontSize='2em';
 		}
 	}
 });
@@ -44,4 +45,4 @@ window.setInterval(function(){
 	toggle_visibility('chairman');
 	toggle_visibility('organized');
 },20000); 
-}
+
