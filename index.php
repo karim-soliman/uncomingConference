2603c24817ad6d460524c107edbade219dcf59e6
computeristgeek <salahuddin.kazak@gmail.com>
2015-11-19 04:52:45 +0200

    Change Navigation Menu to Rects and Update

--- index.php
+++ index.php
@@ -28,44 +28,66 @@
             <div class="col-md-9 col-xs-12" id="main"> 
                 <h1 id="title"><span id="first-conf">1<sup>st</sup> INTERNATIONAL CONFERENCE ON</span><br \><span class="white shaded" id="new-trends">NEW TRENDS FOR SUSTAINABLE ENERGY</span></h1>
                 <h1 id="icntse">ICNTSE</h1>
-                <div class="row shaded"><p class="col-xs-12" id="1-3oct">1-3 October, 2016<br \>At Pharos University, Alexandria, Egypt</p></div>
+		<div class="row shaded">
+		    <p class="col-xs-12" id="1-3oct">1-3 October, 2016<br \>At Pharos University, Alexandria, Egypt</p>
+                </div>
 		<div class="row shaded" id="bottombar">
-                    <p class="col-md-8 col-xs-12" id="organized">Organized by Pharos University in Alexandria with the association of the KTH Royal Institute of Technology </p>
+                    <p class="col-md-8 col-xs-12" id="organized">Organized by Pharos University in Alexandria with the cooperation of the KTH Swedish Royal Institute of Technology </p>
                     <a class="col-md-2 col-xs-6" href="http://www.pua.edu.eg"><img src="img/logo-Arabic.png" /></a>
                     <a class="col-md-2 col-xs-6" href="https://www.kth.se/en"><img src="img/images.png" /></a>
                 </div>
             </div>
 	    <div class="col-md-3 col-xs-12" id="nav">
                     <div class="row">
-			<div class="col-md-12 col-sm-4 col-xs-6" id="about-alex">
-                            <a href="aboutalex.php"><div class="image">
-			    <img src="img/Layer 2.png" />
+			<div class="col-md-12 col-sm-4 col-xs-6 navmenuitem" id="about-alex">
+                            <a href="aboutalex.php">
 			    <p>About Alexandria</p>
-                            </div></a>
+                            </a>
 			</div>
-			<div class="col-md-12 col-sm-4 col-xs-6" id="about-icntse">
-                            <a href="abouticntse.php"><div class="image">
-			    <img src="img/Layer 2 copy.png" />
+			<div class="col-md-12 col-sm-4 col-xs-6 navmenuitem" id="about-icntse">
+                            <a href="abouticntse.php">
 			    <p>About ICNTSE</p>
-                            </div></a>
+                            </a>
 			</div>
-			<div class="col-md-12 col-sm-4 col-xs-6" id="call-for-papers">
-                            <a href="callforpapers.php"><div class="image">
-			    <img src="img/Layer 2 copy 2.png" />
+			<div class="col-md-12 col-sm-4 col-xs-6 navmenuitem" id="brochure">
+                            <a href="brochure.php">
+			    <p>Brochure</p>
+                            </a>
+			</div>
+			<div class="col-md-12 col-sm-4 col-xs-6 navmenuitem" id="programme">
+                            <a href="programme.php">
+			    <p>Programme</p>
+                            </a>
+			</div>
+			<div class="col-md-12 col-sm-4 col-xs-6 navmenuitem" id="keynote">
+                            <a href="keynote.php">
+			    <p>Keynote Speakers</p>
+                            </a>
+			</div>
+			<div class="col-md-12 col-sm-4 col-xs-6 navmenuitem" id="committees">
+                            <a href="comittees.php">
+			    <p>Committees</p>
+                            </a>
+			</div>
+			<div class="col-md-12 col-sm-4 col-xs-6 navmenuitem" id="Sponsors">
+                            <a href="sponsors.php">
+			    <p>Sponsors</p>
+                            </a>
+			</div>
+			<div class="col-md-12 col-sm-4 col-xs-6 navmenuitem" id="call-for-papers">
+                            <a href="callforpapers.php">
 			    <p>Call for Papers</p>
-                            </div></a>
+                            </a>
 			</div>
-			<div class="col-md-12 col-sm-4 col-xs-6" id="fees">
-                            <a href="fees.php"><div class="image">
-			    <img src="img/Layer 2 copy 3.png" />
+			<div class="col-md-12 col-sm-4 col-xs-6 navmenuitem" id="fees">
+                            <a href="fees.php">
 			    <p>Fees</p>
-                            </div></a>
+                            </a>
 			</div>
-			<div class="col-md-12 col-sm-4 col-xs-6" id="contact-us">
-                            <a href="contactus.php"><div class="image">
-			    <img src="img/Layer 2 copy 4.png" />
+			<div class="col-md-12 col-sm-4 col-xs-6 navmenuitem" id="contact-us">
+                            <a href="contactus.php">
 			    <p>Contact Us</p>
-                            </div></a>
+                            </a>
 			</div>
                     </div>
             </div> 
--- js/conf.js
+++ js/conf.js
@@ -1,10 +1,28 @@
 window.addEventListener("resize", function(e) {
 	var icntse=document.getElementById("icntse");
+      	var items= document.getElementsByClassName('navmenuitem');
 	if(window.innerWidth<1000)
 	{
-		icntse.style.letterSpacing='0.125em';
-		if(window.innerWidth<800)
+		if(window.innerWidth>800)
+		{
+			icntse.style.letterSpacing='0.125em';
+			for(i=0; i<items.length; i++) {
+				items[i].style.fontSize='1.5em';
+			}
+		}
+		else
+		{
 			icntse.style.letterSpacing="0em";
+			for(i=0; i<items.length; i++) {
+				items[i].style.fontSize='1em';
+			}
+		}
 	}
-	else icntse.style.letterSpacing='0.5em';
+	else 
+	{
+		icntse.style.letterSpacing='0.5em';
+		for(i=0; i<items.length; i++) {
+			items[i].style.fontSize='2em';
+		}
+	}
 });
