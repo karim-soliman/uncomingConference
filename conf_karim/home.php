01ba516c1c5eea08f30141a93c2e86eefa7fa6bd
Karim Soliman <karim.solimman@gmail.com>
2015-11-20 02:03:32 +0200

    My other point of view for the UNCOMMING WEBSITE

---
+++ conf_karim/home.php
@@ -0,0 +1,262 @@
+<html>
+<head>
+	<title>ICNTSE</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	<style type="text/css">
+	#jumb_head
+	{
+		font-size : 45px;
+	}
+	.jumbotron {
+				background-image: url("img/the-lighthouse.jpg");
+				background-origin: ;
+				background-size: cover;
+			   }
+	mark
+	{
+		background-color: rgba(255,255,255,0.25);
+		color : white;
+	}
+	</style>
+	<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapsed_nav" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">ICNTSE</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="collapsed_nav">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="">About Alexandria</a></li>
+        <li><a href=""></a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Conference <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="">Programme</a></li>
+            <li><a href="">Keynote Speakers</a></li>
+            <li><a href="">Committees</a></li>
+            <li><a href="">Call for papers</a></li>
+            <li><a href="">Fees</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Sponsors</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="">About</a></li>
+          </ul>
+        </li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+     	<li><a href="">Contact us</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<div class="container">
+	<div class="jumbotron">
+	  <h1 id="jumb_head"><mark>1st INTERNATIONAL CONFERENCE ON NEW TRENDS FOR SUSTAINABLE ENERGY<mark></h1>
+	  <p><mark>ICNTSE<br>1-3 October, 2016<br>At Pharos University, Alexandria, Egypt<mark></p>
+	</div>
+</div>
+
+<div class= "container">
+	<hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">About Alexandria. <span class="text-muted">The bride of the mediterranean.</span></h2>
+          <p class="lead">Alexandria is the largest city on the Mediterranean Coast and the second largest city in Egypt, extending about 32 km (20 mi) along the coast. Founded 331 BC by Alexander the Great, it remained the capital of Hellenistic and Roman and Byzantine Egypt for almost 1000 years. Hellenistic Alexandria was best known for the Lighthouse of Alexandria (Pharos), one of the Seven Wonders of the Ancient World</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block img-rounded" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src= "img/alex.jpg">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-9 col-md-push-3">
+          <h2 class="featurette-heading">About ICNTSE.</h2>
+          <p class="lead">The conference is specifically designed to focus on the issues related to energy and energy conservation. Industry leaders, engineers and academics will discuss in this conference the latest developments in the use and production of renewable energy and ways of energy conservation.</p>
+          <h2>The Conference Topics</h2>
+
+			 <div class="panel-group" id="" role="tablist" aria-multiselectable="true">
+			  <div class="panel panel-default">
+			    <div class="panel-heading" role="tab" id="headingOne">
+			      <h4 class="panel-title">
+			        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+			          Power and Energy Engineering
+			        </a>
+			      </h4>
+			    </div>
+			    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+			      <div class="panel-body">
+			      	<ul>
+			      		<li>Innovation in Energy Technology</li>
+			      		<li>Power Systems and Automation</li>
+			      		<li>Centralized and Decentralized Energy Systems</li>
+			      		<li>Power System Quality</li>
+			      		<li>Nuclear Power</li>
+			      		<li>Clean Coal Technologies in Power Generation</li>
+			      		<li>Energy Economics</li>
+			      	</ul>
+			      </div>
+			    </div>
+			  </div>
+			  <div class="panel panel-default">
+			    <div class="panel-heading" role="tab" id="headingTwo">
+			      <h4 class="panel-title">
+			        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+			          Renewable Energy Technologies Applications and Integration
+			        </a>
+			      </h4>
+			    </div>
+			    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+			      <div class="panel-body">
+			      	<ul>
+			      		<li>Hybrid Power Systems</li>
+			      		<li>Photovoltaic Solar Power Systems</li>
+			      		<li>Thermal Solar Energy</li>
+			      		<li>Wind Energy Technology and Applications</li>
+			      		<li>Intelligent Control for Renewable Energy</li>
+			      		<li>Hydrogen fuel</li>
+			      		<li>Fuel Cells Technology and Applicationsli</li>
+			      		<li>Smart Grids, Micro Grids, and others</li>
+			      	</ul>
+			      </div>
+			    </div>
+			  </div>
+			  <div class="panel panel-default">
+			    <div class="panel-heading" role="tab" id="headingThree">
+			      <h4 class="panel-title">
+			        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+			          Energy Management
+			        </a>
+			      </h4>
+			    </div>
+			    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+			      <div class="panel-body">
+			      	<ul>
+			      		<li>Energy and Sustainable Development</li>
+			      		<li>Green Energy Technologies</li>
+			      		<li>Energy and Environment</li>
+			      		<li>Clean Transportation Fuels</li>
+			      		<li>Energy challenges in the urbanization process</li>
+			      		<li>Building Efficiency Technologies</li>
+			      	</ul>
+			      </div>
+			    </div>
+			  </div>
+			   <div class="panel panel-default">
+			    <div class="panel-heading" role="tab" id="headingFour">
+			      <h4 class="panel-title">
+			        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+			         Energy & Sustainable Development
+			        </a>
+			      </h4>
+			    </div>
+			    <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+			      <div class="panel-body">
+			      	<ul>
+			      		<li>Energy Audit and On-site Measurement</li>
+			      		<li>Energy Management & Policies</li>
+			      		<li>Energy Analysis & Energy Efficiency</li>
+			      		<li>Energy Sustainability in developing countries</li>
+			      		<li>National Energy Strategy and Decision making</li>
+			      		<li>Regional Issues, Economics & Policies</li>
+			      	</ul>
+			      </div>
+			    </div>
+			  </div>
+			     <div class="panel panel-default">
+			    <div class="panel-heading" role="tab" id="headingFive">
+			      <h4 class="panel-title">
+			        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+			          BioEnergy
+			        </a>
+			      </h4>
+			    </div>
+			    <div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
+			      <div class="panel-body">
+			      	<ul>
+			      		<li>BioDiesel</li>
+			      		<li>BioFuels</li>
+			      		<li>BioRefinery</li>
+			      	</ul>
+			      </div>
+			    </div>
+			  </div>
+			</div>
+        </div>
+
+        <div class="col-md-3 col-md-pull-9">
+          <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="img/qutim-conference-icon.png">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-10">
+          <h2 class="featurette-heading">Important Dates</h2>
+          <table class="table">
+          	<tr><th>Date</th><th>Event</th></tr>
+          	<tr><td>Feb 15, 2016</td><td>Abstract Submission Deadline</td></tr>
+          	<tr><td>May 20, 2016</td><td>Last Notification for Abstract Acceptance</td></tr>
+          	<tr><td>May 15, 2016</td><td>Full Paper Submission Deadline</td></tr>
+          	<tr><td>July 15, 2016</td><td>Last Notification for Full-Paper Acceptance</td></tr>
+          	<tr><td>May 15, 2016</td><td>Payment Deadline</td></tr>
+          	<tr><td>August 20, 2016</td><td>Letter of Final Acceptance</td></tr>
+          	<tr><td>September 15, 2016</td><td>Conference Program</td></tr>
+          	<tr><td>October 1, 2016</td><td>Conference Launch</td></tr>
+		  </table>
+        </div>
+        <div class="col-md-2">
+          <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="img/calendar-icon.png">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+</div>
+
+
+<div class="container">
+<div class="row">
+	<h1 align="center">Committee</h1>
+ </div>
+      <!-- Three columns of text below circle pictures -->
+      <div class="row">
+        <div class="col-lg-4">
+          <img class="img-circle center-block" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+          <h3 align="center">Chairman</h3>
+          <p align="center">Prof.Dr.Mahmoud ElGammal (PUA)</p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle center-block" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+          <h3 align="center">Co-Chairman</h3>
+          <p align="center">Prof.Dr.Mahmoun Muhammed (KTH)</p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle center-block" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+          <h3 align="center">Secretary General</h3>
+          <p align="center">Prof.Dr.Mohamed Fahiem (PUA)</p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+  </div>
+
+
+
+<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
