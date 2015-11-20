27906b531f69a706277962a3c09e151731446111
computeristgeek <salahuddin.kazak@gmail.com>
2015-11-20 03:38:56 +0200

    Merge with computeristgeek

---
+++ abouticntse.php
@@ -0,0 +1,66 @@
+<?php include 'header.php' ?>
+  <body class="enlargeFonts">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12" id="abouticntse"> 
+		<h1><span class="gray">about</span> <span class="green">ICNTSE</span></h1>
+		<p><span class="green">The conference</span> is specifically designed to focus on the issues related to energy and energy conservation. Industry leaders, engineers and academics will discuss in this conference the latest developments in the use and production of renewable energy and ways of energy conservation.</p>
+		<p><span class="green">Conference Topics:</span><br />The Conference will focus on a number of themes that need prior attention, these themes include:</p>
+            </div> 
+            <div class="col-xs-12 shadedgray" id="icntsefields"> 
+		<ul class="green">
+			<li onclick="toggle_visibility('icntsefield1')">Power and Energy Engineering &#9654;
+			    <ul id="icntsefield1">
+			        <li>Innovation in Energy Technology</li>
+			        <li>Power Systems and Automation</li>
+			        <li>Centralized and Decentralized Energy Systems</li>
+			        <li>Power System Quality</li>
+			        <li>Nuclear Power</li>
+			        <li>Clean Coal Technologies in Power Generation</li>
+			        <li>Energy Economics</li>
+			    </ul>
+			</li>
+			<li onclick="toggle_visibility('icntsefield2')">Renewable Energy Technologies Applications and Integration &#9654;
+			    <ul id="icntsefield2">
+			        <li>Hybrid Power Systems</li>
+			        <li>Photovoltaic Solar Power Systems</li>
+			        <li>Thermal Solar Energy</li>
+			        <li>Wind Energy Technology and Applications</li>
+			        <li>Intelligent Control for Renewable Energy</li>
+			        <li>Hydrogen fuel</li>
+			        <li>Fuel Cells Technology and Applications</li>
+			        <li>Smart Grids, Micro Grids, and others</li>
+			    </ul>
+			</li>
+			<li onclick="toggle_visibility('icntsefield3')">Energy Management &#9654;
+			    <ul id="icntsefield3">
+			        <li>Energy and Sustainable Development</li>
+			        <li>Green Energy Technologies</li>
+			        <li>Energy and Environment</li>
+			        <li>Clean Transportation Fuels</li>
+			        <li>Energy challenges in the urbanization process</li>
+			        <li>Building Efficiency Technologies</li>
+			    </ul>
+			</li>
+			<li onclick="toggle_visibility('icntsefield4')">Energy & Sustainable Development &#9654;
+			    <ul id="icntsefield4">
+			        <li>Energy Audit and On-site Measurement</li>
+			        <li>Energy Management & Policies</li>
+			        <li>Energy Analysis & Energy Efficiency</li>
+			        <li>Energy Sustainability in developing countries</li>
+			        <li>National Energy Strategy and Decision making</li>
+			        <li>Regional Issues, Economics & Policies</li>
+			    </ul>
+			</li>
+			<li onclick="toggle_visibility('icntsefield5')">BioEnergy &#9654;
+			    <ul id="icntsefield5">
+			        <li>BioDiesel</li>
+			        <li>BioFuels</li>
+			        <li>BioRefinery</li>
+			    </ul>
+			</li>
+		</ul>
+            </div> 
+        </div>	
+    </div>
+<?php include 'footer.php' ?>
