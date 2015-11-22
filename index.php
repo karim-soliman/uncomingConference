75835f1d13742f8a4cf8299a4f950ddd5c2f7b73
computeristgeek <salahuddin.kazak@gmail.com>
2015-11-22 23:39:28 +0200

    Resolve index.php undetected conflict

--- index.php
+++ index.php
@@ -25,12 +25,12 @@
                 <h2 class="visible-xs visible-sm" id="title"><span id="first-conf">1<sup>st</sup> INTERNATIONAL CONFERENCE ON</span><br \><span class="white shaded" id="new-trends">NEW TRENDS FOR SUSTAINABLE ENERGY</span></h1>
                 <h1 id="icntse">ICNTSE</h1>
 		<div class="row shaded">
-		    <p class="col-xs-12" id="1-3oct">1-3 October, 2016<br \>At Pharos University, Alexandria, Egypt</p>
+		    <p class="col-xs-12 col-md-7" id="chairman2">Chairman: Prof.Mahmoud ElGammal (PUA)<br />Co-Chairman: Prof.Mamoun Muhammed (KTH)<br />Secretary General: Prof.Mohamed Fahim (PUA)</p>
+		    <p class="col-xs-12 col-md-5" id="1-3oct">1-3 October, 2016<br \>At Pharos University, Alexandria, Egypt</p>
                 </div>
 		<div class="row">
 		<div class="shaded" id="bottombar">
                     <p class="col-xs-12 col-md-8" id="organized" style="display:inline;">Organized by Pharos University in Alexandria with the cooperation of the KTH Swedish Royal Institute of Technology </p>
-		    <p class="col-xs-12 col-md-8" id="chairman" style="display:none;">Chairman: Prof.Dr.Mahmoud ElGammal (PUA), <br />Co-Chairman: Prof.Dr.Mahmoun Muhammed (KTH), Secretary General: Prof.Dr.Mohamed Fahiem (PUA)</p>
                     <a class="col-md-2 col-xs-6" href="http://www.pua.edu.eg"><img src="img/logo-Arabic.png" /></a>
                     <a class="col-md-2 col-xs-6" href="https://www.kth.se/en"><img src="img/images.png" /></a>
                 </div> 
