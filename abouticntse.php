f4834672a5e20873f651c1e58b5f92c68e8c4e9a
computeristgeek <salahuddin.kazak@gmail.com>
2015-11-23 22:24:33 +0200

    Fix number margins

--- abouticntse.php
+++ abouticntse.php
@@ -10,7 +10,7 @@
             <div class="col-xs-12 shadedgray" id="icntsefields"> 
 		<ul class="green">
 			<li onclick="toggle_visibility('icntsefield1')">Power and Energy Engineering &#9654;
-			    <ul id="icntsefield1">
+			    <ol id="icntsefield1">
 			        <li>Innovation in Energy Technology</li>
 			        <li>Power Systems and Automation</li>
 			        <li>Centralized and Decentralized Energy Systems</li>
@@ -18,10 +18,10 @@
 			        <li>Nuclear Power</li>
 			        <li>Clean Coal Technologies in Power Generation</li>
 			        <li>Energy Economics</li>
-			    </ul>
+			    </ol>
 			</li>
 			<li onclick="toggle_visibility('icntsefield2')">Renewable Energy Technologies Applications and Integration &#9654;
-			    <ul id="icntsefield2">
+			    <ol id="icntsefield2">
 			        <li>Hybrid Power Systems</li>
 			        <li>Photovoltaic Solar Power Systems</li>
 			        <li>Thermal Solar Energy</li>
@@ -30,34 +30,34 @@
 			        <li>Hydrogen fuel</li>
 			        <li>Fuel Cells Technology and Applications</li>
 			        <li>Smart Grids, Micro Grids, and others</li>
-			    </ul>
+			    </ol>
 			</li>
 			<li onclick="toggle_visibility('icntsefield3')">Energy Management &#9654;
-			    <ul id="icntsefield3">
+			    <ol id="icntsefield3">
 			        <li>Energy and Sustainable Development</li>
 			        <li>Green Energy Technologies</li>
 			        <li>Energy and Environment</li>
 			        <li>Clean Transportation Fuels</li>
 			        <li>Energy challenges in the urbanization process</li>
 			        <li>Building Efficiency Technologies</li>
-			    </ul>
+			    </ol>
 			</li>
 			<li onclick="toggle_visibility('icntsefield4')">Energy & Sustainable Development &#9654;
-			    <ul id="icntsefield4">
+			    <ol id="icntsefield4">
 			        <li>Energy Audit and On-site Measurement</li>
 			        <li>Energy Management & Policies</li>
 			        <li>Energy Analysis & Energy Efficiency</li>
 			        <li>Energy Sustainability in developing countries</li>
 			        <li>National Energy Strategy and Decision making</li>
 			        <li>Regional Issues, Economics & Policies</li>
-			    </ul>
+			    </ol>
 			</li>
 			<li onclick="toggle_visibility('icntsefield5')">BioEnergy &#9654;
-			    <ul id="icntsefield5">
+			    <ol id="icntsefield5">
 			        <li>BioDiesel</li>
 			        <li>BioFuels</li>
 			        <li>BioRefinery</li>
-			    </ul>
+			    </ol>
 			</li>
 		</ul>
             </div> 
