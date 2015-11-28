dcf790b8a0637d92ae6220a25e984f3dc0e6a118
computeristgeek <salahuddin.kazak@gmail.com>
2015-11-28 18:53:47 +0200

    Build Registration page

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 		<div class="row">
 		<div class="bottombar">
 		    <div class="col-xs-12">
-			<div class="col-xs-12 col-sm-4"><a href="#"><div class="regButtons">Registration</div></a></div>
+			<div class="col-xs-12 col-sm-4"><a href="registration.php"><div class="regButtons">Registration</div></a></div>
 			<div class="invisibile-xs col-sm-4"></div>
 			<div class="col-xs-12 col-sm-4"><a href="#"><div class="regButtons">Abstract Submission</div></a></div>
 		    </div>
--- registration.php
+++ registration.php
@@ -7,6 +7,102 @@
 		</div>
 	    </div>
     <hr>
-	    <img class="center-block" src="img/under-construction-150271_1280.png" />
+	<form class="form-horizontal" method="post" action="" enctype="multipart/form-data" id="form_maker">
+			<fieldset>
+					<div class="form-group">
+					  <label class="col-md-4 control-label">Full Name:<span style="color:#E83538">*</span></label>  
+					  <div class="col-md-4">
+					  <input id="textinput" name="fullname" type="text" class="form-control input-md" placeholder="">
+					  </div>
+					</div>
+					<div class="form-group">
+					  <label class="col-md-4 control-label">Title:<span style="color:#E83538">*</span></label>
+					  <div class="col-md-4">
+						<select id="selectbasic" name="title" class="form-control"><option value="">Select Option</option><option value="Mr.">Mr.</option><option value=" Ms."> Ms.</option><option value=" Dr."> Dr.</option><option value=" Prof."> Prof.</option></select>
+					  </div>
+					</div>
+					<div class="form-group">
+					  <label class="col-md-4 control-label">Affiliation:<span style="color:#E83538">*</span></label>  
+					  <div class="col-md-4">
+					  <input id="textinput" name="affiliation" type="text" class="form-control input-md" placeholder="">
+					  </div>
+					</div>
+					<div class="form-group">
+					  <label class="col-md-4 control-label">Address:</label>  
+					  <div class="col-md-4">
+					  <input id="textinput" name="address" type="text" class="form-control input-md" placeholder="">
+					  </div>
+					</div>
+					<div class="form-group">
+					  <label class="col-md-4 control-label">City:</label>  
+					  <div class="col-md-4">
+					  <input id="textinput" name="city" type="text" class="form-control input-md" placeholder="">
+					  </div>
+					</div>
+					<div class="form-group">
+					  <label class="col-md-4 control-label">Country:<span style="color:#E83538">*</span></label>  
+					  <div class="col-md-4">
+					  <input id="textinput" name="country" type="text" class="form-control input-md" placeholder="">
+					  </div>
+					</div>
+					<div class="form-group">
+					  <label class="col-md-4 control-label">Postal/Zip Code:</label>  
+					  <div class="col-md-4">
+					  <input id="textinput" name="postal_zip" type="text" class="form-control input-md" placeholder="">
+					  </div>
+					</div>
+					<div class="form-group">
+					  <label class="col-md-4 control-label">Phone:<span style="color:#E83538">*</span></label>  
+					  <div class="col-md-4">
+					  <input id="textinput" name="phone" type="phone" class="form-control input-md" placeholder="">
+					  </div>
+					</div>
+					<div class="form-group">
+					  <label class="col-md-4 control-label">Email:<span style="color:#E83538">*</span></label>  
+					  <div class="col-md-4">
+					  <input id="textinput" name="email" type="email" class="form-control input-md" placeholder="">
+					  </div>
+					</div>
+					<div class="form-group">
+					  <label class="col-md-4 control-label">I intend to :</label>
+					  <div class="col-md-4">
+							 <div class="checkbox">
+								<label style="text-align: left;">
+								  <input type="checkbox" name="intend_to[]" value="Participate" id="intend_to1"> Participate</label>
+								</div>
+							 <div class="checkbox">
+								<label style="text-align: left;">
+								  <input type="checkbox" name="intend_to[]" value=" Present a poster communication" id="intend_to2">  Present a poster communication</label>
+								</div>
+							 <div class="checkbox">
+								<label style="text-align: left;">
+								  <input type="checkbox" name="intend_to[]" value=" Present an oral communication" id="intend_to3">  Present an oral communication</label>
+								</div>
+							 <div class="checkbox">
+								<label style="text-align: left;">
+								  <input type="checkbox" name="intend_to[]" value=" Participant" s="" guest'="" id="intend_to4">  Participant's guest</label>
+								</div>
+							 <div class="checkbox">
+								<label style="text-align: left;">
+								  <input type="checkbox" name="intend_to[]" value=" Exhibitor" id="intend_to5">  Exhibitor</label>
+								</div>
+					  </div>
+					</div>
+					<div class="form-group">
+					  <label class="col-md-4 control-label">Please indicate the title of your oral/poster communication:</label>
+					  <div class="col-md-4">                     
+						<textarea class="form-control" id="textarea" name="poster" placeholder=""></textarea>
+					  </div>
+					</div>
+					<div class="form-group" id="file_div" style="display: none;">
+					  <label class="col-md-4 control-label">Upload File : </label>  
+					  <div class="col-md-4">
+					  <input id="file" name="file" type="file" class="form-control input-md">
+					  </div>
+					</div><div class="form-group">
+						<label class="col-md-4 control-label"></label>
+						<div class="col-md-4"><button id="singlebutton" name="save_data" class="btn btn-primary">Submit</button></div>
+			  </div>
+			</fieldset></form>
     </div>
 <?php include 'footer.php' ?>
