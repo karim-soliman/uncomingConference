2a2f1b30da345a036681a1d321c255de1243b24a
Karim Soliman <karim.solimman@gmail.com>
2015-12-02 21:44:31 +0200

    Contact us modal in separate file to be included

---
+++ contactus_modal.php
@@ -0,0 +1,24 @@
+<!--Modal-->
+    <div id="contact-us" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+        <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	     <h1 class="modal-title">Contact us</h1>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="col-md-8">
+            <h3 class="">Conference Email: ICNTSE@pua.edu.eg</h3>
+            <h3 class="">Conference Website: <a href="http://www.icntse.com">http://www.icntse.com</a></h3>
+            <h3 class="">Telephone: +203-3877-(553|453)</h3>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13649.958643189657!2d29.960322!3d31.2071607!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6aa3847bf78e707d!2sPharos+University+in+Alexandria!5e0!3m2!1sen!2seg!4v1448479139696" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+          </div>
+          <div class="col-md-3"><img src="img/contactus.png"></div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</div>
+    <!--End modal -->
