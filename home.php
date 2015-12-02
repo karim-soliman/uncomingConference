37977afe83f66d025eb3b8622272b80e630410db
Karim Soliman <karim.solimman@gmail.com>
2015-12-02 21:41:43 +0200

    Updates on Home.php [kter awe msh faker]

--- home.php
+++ home.php
@@ -94,27 +94,66 @@
       <divclass="row featurette">
         <div class="col-md-7">
           <h2 class="featurette-heading">About Alexandria. <span class="text-muted">The bride of the mediterranean.</span></h2>
-          <p class="lead">Alexandria is the largest city on the Mediterranean Coast and the second largest city in Egypt, extending about 32 km (20 mi) along the coast. Founded 331 BC by Alexander the Great, it remained the capital of Hellenistic and Roman and Byzantine Egypt for almost 1000 years. Hellenistic Alexandria was best known for the Lighthouse of Alexandria (Pharos), one of the Seven Wonders of the Ancient World</p>
+          <p class="lead">Alexandria is the largest city on the Mediterranean Coast and the second largest city in Egypt, extending about 32 km (20 mi) along the coast. Founded 331 BC by Alexander the Great, it remained the capital of Hellenistic and Roman and Byzantine Egypt for almost 1000 years. Hellenistic Alexandria was best known for the Lighthouse of Alexandria (Pharos), one of the Seven Wonders of the Ancient World ... <a class="btn btn-info btn-sm" href="aboutalex.php" role="button">Read more</a></p>
         </div>
         <div class="col-md-5">
-          <img class="featurette-image img-responsive center-block img-rounded" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src= "img/alex.jpg">
-        </div>
+          <!-- Alexandria carousal -->
+          <div id = "alex_carousal" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="img/alex.jpg">
+      <div class="carousel-caption">
+        El Raml Station
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/AlexandriaLibrary2.jpg">
+      <div class="carousel-caption">
+        Bibliotheca Alexandria 
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/stanley.jpg">
+      <div class="carousel-caption">
+        Stanley Bridge   
+      </div>
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#alex_carousal" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#alex_carousal" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div> <!-- end of carousal Alexandria -->
+        </div> <!-- end of col-md-5 -->
       </div>
 
+      <div  id="confTopics"></div>
       <hr class="featurette-divider">
 
-      <div class="row featurette">
+      <div  class="row featurette">
         <div class="col-md-9 col-md-push-3">
-          <h2 class="featurette-heading">About ICNTSE.</h2>
+          <h2 class="featurette-heading">The Conference Topics.</h2>
           <p class="lead">The conference is specifically designed to focus on the issues related to energy and energy conservation. Industry leaders, engineers and academics will discuss in this conference the latest developments in the use and production of renewable energy and ways of energy conservation.</p>
-          <h2>The Conference Topics</h2>
-
 			 <div class="panel-group" id="" role="tablist" aria-multiselectable="true">
 			  <div class="panel panel-default">
 			    <div class="panel-heading" role="tab" id="headingOne">
 			      <h4 class="panel-title">
 			        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-			          Power and Energy Engineering
+			          <b>Power and Energy Engineering</b>
 			        </a>
 			      </h4>
 			    </div>
@@ -136,7 +175,7 @@
 			    <div class="panel-heading" role="tab" id="headingTwo">
 			      <h4 class="panel-title">
 			        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-			          Renewable Energy Technologies Applications and Integration
+			          <b>Renewable Energy Technologies Applications and Integration</b>
 			        </a>
 			      </h4>
 			    </div>
@@ -159,7 +198,7 @@
 			    <div class="panel-heading" role="tab" id="headingThree">
 			      <h4 class="panel-title">
 			        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-			          Energy Management
+			          <b>Energy Management</b>
 			        </a>
 			      </h4>
 			    </div>
@@ -180,7 +219,7 @@
 			    <div class="panel-heading" role="tab" id="headingFour">
 			      <h4 class="panel-title">
 			        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-			         Energy & Sustainable Development
+			         <b>Energy & Sustainable Development</b>
 			        </a>
 			      </h4>
 			    </div>
@@ -201,7 +240,7 @@
 			    <div class="panel-heading" role="tab" id="headingFive">
 			      <h4 class="panel-title">
 			        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-			          BioEnergy
+			          <b>BioEnergy</b>
 			        </a>
 			      </h4>
 			    </div>
@@ -215,6 +254,23 @@
 			      </div>
 			    </div>
 			  </div>
+        <div class="panel panel-default">
+          <div class="panel-heading" role="tab" id="headingSix">
+            <h4 class="panel-title">
+              <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                <b>Energy Sotrage</b>
+              </a>
+            </h4>
+          </div>
+          <div id="collapseSix" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSix">
+            <div class="panel-body">
+              <ul>
+                <li>Physical Energy Storage</li>
+                <li>Chemical Energy Storage</li>
+              </ul>
+            </div>
+          </div>
+        </div>
 			</div>
         </div>
 
@@ -222,7 +278,7 @@
           <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="img/qutim-conference-icon.png">
         </div>
       </div>
-
+      <div id = "callPapers"></div>
       <hr class="featurette-divider">
 
       <div class="row featurette">
@@ -230,47 +286,69 @@
           <h2 class="featurette-heading">Important Dates</h2>
           <table class="table">
           	<tr><th>Date</th><th>Event</th></tr>
-          	<tr><td>Feb 15, 2016</td><td>Abstract Submission Deadline</td></tr>
-          	<tr><td>May 20, 2016</td><td>Last Notification for Abstract Acceptance</td></tr>
+          	<tr><td>February 15, 2016</td><td>Abstract Submission Deadline</td></tr>
+          	<tr><td>March 20, 2016</td><td>Last Notification for Abstract Acceptance*</td></tr>
           	<tr><td>May 15, 2016</td><td>Full Paper Submission Deadline</td></tr>
-          	<tr><td>July 15, 2016</td><td>Last Notification for Full-Paper Acceptance</td></tr>
-          	<tr><td>May 15, 2016</td><td>Payment Deadline</td></tr>
-          	<tr><td>August 20, 2016</td><td>Letter of Final Acceptance</td></tr>
+          	<tr><td>July 15, 2016</td><td>Last Notification for Full-Paper Acceptance**</td></tr>
+          	<tr><td>August 15, 2016</td><td>Payment Deadline</td></tr>
+          	<tr><td>August 31, 2016</td><td>Letter of Final Acceptance***</td></tr>
           	<tr><td>September 15, 2016</td><td>Conference Program</td></tr>
           	<tr><td>October 1, 2016</td><td>Conference Launch</td></tr>
 		  </table>
+      <h4>
+        *Acceptance is issued once a positive feedback is received from the reviewing committee<br>
+        **Late payment fees are mandatory after this date<br>
+        ***Will not be issued without completing the fees, and approval of the Full-Paper
+      </h4>
         </div>
         <div class="col-md-2">
           <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="img/calendar-icon.png">
         </div>
       </div>
-
+      <div id="venue"></div>
       <hr class="featurette-divider">
-
 </div>
 
+  <div class="row">
+    <div class="col-md-7">
+      <h1>Venue & Accommodation</h1>
+      <h4>
+        The venue of this conference is Alexandria "The Mediterranean Pearl". It is a thriving city, rich in beauty, heritage, culture and history as it has been Egypt's prime gate throughout history that prevailed for centuries as the world's greatest repository of knowledge and exporter of culture. We look forward to seeing you this October in our beautiful city. 
+
+        <br><br>Special rates for the accommodation for our participants are in:
+        <br>
+        <ul>
+          <li><h4>Hilton Green Plaza: <a class="btn btn-sm btn-info" href="http://www.ar.hilton.com/en/hotel/Hilton-Alexandria-Green-Plaza">Click here</a></h4></li>
+          <li><h4>Green Plaza Inn: <a class="btn btn-sm btn-info" href="http://www.greenplazainn.com/">Click here</a></h4></li>
+        </ul>
+      </h4>
+    </div>
+    <div class="col-md-4"><img src="img/hilton.jpg" class="img-responsive img-thumbnail"></div>
+  </div>
+<hr>
 
 <div class="container">
+  <div id="sponsers"></div>
 <div class="row">
-	<h1 align="center">Committee</h1>
+	<h1 align="center">Sponsers</h1>
  </div>
       <!-- Three columns of text below circle pictures -->
       <div class="row">
-        <div class="col-lg-4">
-          <img class="img-circle center-block" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h3 align="center">Chairman</h3>
-          <p align="center">Prof.Dr.Mahmoud ElGammal (PUA)</p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-          <img class="img-circle center-block" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h3 align="center">Co-Chairman</h3>
-          <p align="center">Prof.Dr.Mahmoun Muhammed (KTH)</p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-          <img class="img-circle center-block" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h3 align="center">Secretary General</h3>
-          <p align="center">Prof.Dr.Mohamed Fahiem (PUA)</p>
-        </div><!-- /.col-lg-4 -->
+        <div class="col-md-4">
+          <img class="img-circle center-block" src="img/logo.png" width="140" height="140">
+          <h3 align="center">Schenider</h3>
+          
+        </div><!-- /.col-md-4 -->
+        <div class="col-md-4">
+          <img class="img-circle center-block" src="img/logo.png" width="140" height="140">
+          <h3 align="center">ABB</h3>
+          
+        </div><!-- /.col-md-4 -->
+        <div class="col-md-4">
+          <img class="img-circle center-block" src="img/logo.png" width="140" height="140">
+          <h3 align="center">Sponser 3</h3>
+         
+        </div><!-- /.col-md-4 -->
       </div><!-- /.row -->
   </div>
 
