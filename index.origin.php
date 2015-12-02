dbebcbb2ac8066d687dee7e403ec233ffd347775
computeristgeek <salahuddin.kazak@gmail.com>
2015-12-03 01:43:28 +0200

    Add index.php backup

---
+++ index.origin.php
@@ -0,0 +1,33 @@
+<?php include 'header.php' ?>
+  <body style="padding: 50px;">
+<?php include('navbar.php');?>
+<?php include('contactus_modal.php') ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12" id="main"> 
+                <h1 class="visible-md visible-lg" id="title"><span id="first-conf">1<sup>st</sup> INTERNATIONAL CONFERENCE ON</span><br \><span class="white shaded" id="new-trends">NEW TRENDS FOR SUSTAINABLE ENERGY</span></h1>
+                <h2 class="visible-xs visible-sm" id="title"><span id="first-conf">1<sup>st</sup> INTERNATIONAL CONFERENCE ON</span><br \><span class="white shaded" id="new-trends">NEW TRENDS FOR SUSTAINABLE ENERGY</span></h1>
+                <h1 id="icntse">ICNTSE</h1>
+		<div class="row shaded">
+		    <p class="col-xs-12 col-md-7" id="chairman2">Chairman: Prof.Mahmoud ElGammal (PUA)<br />Co-Chairman: Prof.Mamoun Muhammed (KTH)<br />Secretary General: Prof.Mohamed Fahim (PUA)</p>
+		    <p class="col-xs-12 col-md-5" id="_1-3oct">1-3 October, 2016<br \>At Pharos University, Alexandria, Egypt</p>
+                </div>
+		<div class="row">
+		<div class="bottombar">
+		    <div class="col-xs-12">
+			<div class="col-xs-12 col-sm-4"><a href="registration.php"><div class="regButtons">Registration</div></a></div>
+			<div class="invisibile-xs col-sm-4"></div>
+			<div class="col-xs-12 col-sm-4"><a href="abstract.php"><div class="regButtons">Abstract Submission</div></a></div>
+		    </div>
+		    <div class="col-xs-12 shaded">
+			    <p class="col-xs-12 col-md-8" id="organized" style="display:inline;">Organized by Pharos University in Alexandria with the cooperation of the KTH Swedish Royal Institute of Technology </p>
+			    <div class="col-xs-6 col-md-2"><a href="http://www.pua.edu.eg"><img src="img/logo-Arabic.png" /></a></div>
+			    <div class="col-xs-6 col-md-2"><a href="https://www.kth.se/en"><img src="img/images.png" /></a></div>
+		    </div> 
+		</div>
+		</div>
+            </div>
+                    </div>
+        </div>	
+<?php include 'footer.php' ?>
+    </div>
