985da38eeac677c31c00ba0ce937b06f4941a116
computeristgeek <salahuddin.kazak@gmail.com>
2015-12-03 01:50:22 +0200

    Add Karim's version to backup

---
+++ index.k.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+	<title>ICNTSE</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/cover.css">
+	<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+</head>
+<body style="background: url(img/solar_energy.jpg) no-repeat center center fixed;">
+  <?php include('contactus_modal.php');?>
+
+ <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <nav>
+                 <h3 class="masthead-brand"><img src="img/pua.png"> <img src="img/kth.png"></h3>
+                <ul class="nav masthead-nav">
+                  <li class="active"><a href="#">Home</a></li>
+                  <li><a href="registration.php">Registration</a></li>
+                  <li><a href="contactus.php">Contact</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <h1 class="cover-heading">INTERNATIONAL CONFERENCE ON NEW TRENDS FOR SUSTAINABLE ENERGY (ICNTSE)</h1>
+            <p class="lead">Chairman: Prof.Mahmoud ElGammal (PUA)<br>
+                            Co-Chairman: Prof.Mamoun Muhammed (KTH)<br>
+                            Secretary General: Prof.Mohamed Fahim (PUA)</p>
+                            <p class="lead">1-3 October, 2016<br>
+                                  At Pharos University, Alexandria, Egypt</p>
+              <p class="lead"><a class="btn btn-lg btn-info" href="home.php">Learn
+                more</a></p>
+            </p>
+          </div>
+
+          <div class="mastfoot">
+            <div class="inner">
+              <p><font color="black">Conference hosted by </font><a href="http://www.pua.edu.eg">PUA</a>. 2016</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
