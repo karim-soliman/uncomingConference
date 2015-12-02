4c4a99428a05e06e15d904ed89f1e72e4bbb9097
Karim Soliman <karim.solimman@gmail.com>
2015-12-02 21:43:32 +0200

    updates the navbar [include the contact us modal to be added automatically ] - All links works

--- navbar.php
+++ navbar.php
@@ -15,27 +15,32 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="collapsed_nav">
       <ul class="nav navbar-nav">
-        <li><a href="#alex">About Alexandria</a></li>
-        <li><a href=""></a></li>
+        <li><a href="home.php">Home</a></li>
+        <li><a href="aboutalex.php">About Alexandria</a></li>
+        <li><a href="registration.php">Registration</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Conference <span class="caret"></span></a>
           <ul class="dropdown-menu">
+            <li><a href="home.php#confTopics">Topics</a></li>
             <li><a href="programme.php">Programme</a></li>
             <li><a href="keynote.php">Keynote Speakers</a></li>
             <li><a href="committees.php">Committees</a></li>
-            <li><a href="callforpapers.php">Call for papers</a></li>
+            <li><a href="home.php#callPapers">Call for papers</a></li>
             <li><a href="fees.php">Fees</a></li>
+            <li><a href="poster.php">Poster Guidelines</a></li>
             <li role="separator" class="divider"></li>
-            <li><a href="sponsors.php">Sponsors</a></li>
+            <li><a href="home.php#sponsers">Sponsors</a></li>
             <li role="separator" class="divider"></li>
-            <li><a href="topics.php">About</a></li>
+            <li><a href="home.php#venue">Venue & Accommodation</a></li>
           </ul>
         </li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-     	<li><a href="">Contact us</a></li>
+     	<li><a href="" data-toggle="modal" data-target="#contact-us">Contact us</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
 <!-- end of nav -->
+
+<?php include('contactus_modal.php');?>
