7c13535ccd04ddcace9b117e612e138eaa143c68
Karim Soliman <karim.solimman@gmail.com>
2015-12-05 16:41:00 +0200

    adding css file to header

--- header.php
+++ header.php
@@ -12,7 +12,8 @@
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
     <!-- Style -->
     <link href="css/style.css" rel="stylesheet">
-
+    <link rel="stylesheet" type="text/css" href="css/btn_3d.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
