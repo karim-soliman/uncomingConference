4934a9f4b40c8a3b7da71a7795791f518de0bdc7
computeristgeek <salahuddin.kazak@gmail.com>
2015-12-06 05:12:57 +0200

    Vertical-Align Cover Text Middle

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
                 <h1 class="visible-md visible-lg" id="title"><span id="first-conf">1<sup>st</sup> INTERNATIONAL CONFERENCE ON</span><br \><span class="white shaded" id="new-trends">NEW TRENDS FOR SUSTAINABLE ENERGY</span></h1>
                 <h2 class="visible-xs visible-sm" id="title"><span id="first-conf">1<sup>st</sup> INTERNATIONAL CONFERENCE ON</span><br \><span class="white shaded" id="new-trends">NEW TRENDS FOR SUSTAINABLE ENERGY</span></h1>
                 <h1 id="icntse">ICNTSE</h1>
-		<div class="row shaded">
-		    <p class="col-xs-12 col-md-7" id="chairman2">Chairman: Prof.Mahmoud ElGammal (PUA)<br />Co-Chairman: Prof.Björn Palm (KTH)<br />Co-Chairman: Prof.Mamoun Muhammed (KTH)<br />Secretary General: Prof.Mohamed Fahim (PUA)</p>
-		    <p class="col-xs-12 col-md-5" id="_1-3oct">1-3 October, 2016<br \>At Pharos University, Alexandria, Egypt</p>
+		<div class="row">
+		    <div class="col-xs-12 col-md-7 shaded" id="chairman2"><span>Chairman: Prof.Mahmoud ElGammal (PUA)<br />Co-Chairman: Prof.Björn Palm (KTH)<br />Co-Chairman: Prof.Mamoun Muhammed (KTH)<br />Secretary General: Prof.Mohamed Fahim (PUA)</span></div>
+		    <div class="col-xs-12 col-md-5 shaded" id="_1-3oct"><span>1-3 October, 2016<br \>At Pharos University, Alexandria, Egypt</span></div>
                 </div>
 		<div class="row">
 		<div class="bottombar">
