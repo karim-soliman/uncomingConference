8634a5498d9fef629bffec12b53998d12a030544
computeristgeek <salahuddin.kazak@gmail.com>
2015-12-06 04:13:06 +0200

    Fix Map Responsiveness

--- contactus_modal.php
+++ contactus_modal.php
@@ -12,7 +12,7 @@
             <h3 class="">Conference Email: ICNTSE@pua.edu.eg</h3>
             <h3 class="">Conference Website: <a href="http://www.icntse.com">http://www.icntse.com</a></h3>
             <h3 class="">Telephone: +203-3877-(553|453)</h3>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13649.958643189657!2d29.960322!3d31.2071607!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6aa3847bf78e707d!2sPharos+University+in+Alexandria!5e0!3m2!1sen!2seg!4v1448479139696" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13649.958643189657!2d29.960322!3d31.2071607!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6aa3847bf78e707d!2sPharos+University+in+Alexandria!5e0!3m2!1sen!2seg!4v1448479139696" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
           </div>
           <div class="col-md-3"><img src="img/contactus.png"></div>
         </div>
